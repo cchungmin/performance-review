eb3e35b3dac9f68bc3e2c16d7d3f31826826c397
cchungmin <min427@gmail.com>
2019-09-05 02:47:29 +0900

    CRUD Apis

---
+++ actions/api.js
@@ -0,0 +1,6 @@
+export default {
+  FETCH_ALL_EMPLOYEES: '/v1/api/fetch-employees',
+  ADD_EMPLOYEE: '/v1/api/add-employee',
+  UPDATE_EMPLOYEE: '/v1/api/update-employee',
+  DELETE_EMPLOYEE: '/v1/api/delete-employee',
+}
